0338269bf35a01b2c02e3d23d544a8e62711faad
Mirror83 <glenochieng045@gmail.com>
2024-12-25 06:02:06 +0300

    chore: removed evaluate procedure and extern declaration
    The correct evaluate procedure is in recursive_descent.h, and that extern struct declaration is not needed since we are not keeping a global list to hold the tokens anymore.

--- assignment-2/recursive_descent.c
+++ assignment-2/recursive_descent.c
@@ -1,24 +1,10 @@
 #include "recursive_descent.h"
 
-extern struct Token tokens[MAX_TOKENS];
-
-void evaluate(char *cmdline)
-{
-    if (strcmp(cmdline, "quit\n") == 0)
-    {
-        exit(EXIT_SUCCESS);
-    }
-
-    tokenize(cmdline, tokens);
-    int result = parse_P();
-    printf("%d\n", result);
-}
-
 void print_header()
 {
     printf("Recusive descent calculator. (+, and *)\n");
     printf("---------------------------------------\n");
-    printf("Enter 'quit' or 'CTR+D' and then press enter to exit.\n\n");
+    printf("Enter 'quit' or 'CTR+D' and then press Enter to exit.\n\n");
 }
 
 int main(int argc, char **argv)
